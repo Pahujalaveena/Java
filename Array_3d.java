f470cf4a518379bdc70c7034edb95f9d55a8855e
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2023-02-19 07:59:53 +0000

    Add files via upload

---
+++ Array_3d.java
@@ -0,0 +1,11 @@
+package Basic;
+
+public class Array_3d {
+
+	public static void main(String args[]) {
+		
+		int[][][] arr= {{{1,2,3},{3,4,5}},{{1,4,3},{4,5,3}}};
+		
+		System.out.println("Element on arr[1][1][1] index " +arr[1][2][3]);
+	}
+}
