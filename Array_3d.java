8e22b5193fd10a4e3f9ba7ddf2d3a03f80fb28a8
Laveena Pahuja <laveenapahuja5@gmail.com>
2023-10-07 03:34:26 +0100

    new commit

--- Array_3d.java
+++ Array_3d.java
@@ -4,8 +4,9 @@ public class Array_3d {
 
 	public static void main(String args[]) {
 		
-		int[][][] arr= {{{1,2,3},{3,4,5}},{{1,4,3},{4,5,3}}};
+		int[][][] arr= {{{1,2,3},{3,4,5}},{{1,4,3},{4,5,8}}};
 		
 		System.out.println("Element on arr[1][1][1] index " +arr[1][2][3]);
 	}
 }
+//new comit
