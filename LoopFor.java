77bda74b8027f507529c5dd7cf3c0a7b1f58e1d8
Laveena Pahuja <113932098+Pahujalaveena@users.noreply.github.com>
2024-03-04 19:58:14 +0000

    Add files via upload

---
+++ LoopFor.java
@@ -0,0 +1,16 @@
+package Java_Tutorial;
+
+public class LoopFor {
+	
+	public static void main(String arg[])
+	{
+		int n = 7;
+		
+		System.out.println("The table of "+n+" is:");
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n+" * "+i+" = "+n*i);
+		}
+	}
+
+}
