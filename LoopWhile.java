bc101349aa2cb7a0e49f86a4c8e5d9816e43ec60
Laveena Pahuja <113932098+Pahujalaveena@users.noreply.github.com>
2024-03-06 14:04:34 +0000

    Add files via upload

---
+++ LoopWhile.java
@@ -0,0 +1,18 @@
+package Java_Tutorial;
+
+public class LoopWhile {
+
+	public static void main(String[] args) {
+
+		int n=9;
+		System.out.println("The table of "+n+" is ");
+		
+		int i=1;
+		while(i<=10)
+		{
+			System.out.println(n+" * "+" i "+" = "+n*i);
+			i++;
+		}
+	}
+
+}
